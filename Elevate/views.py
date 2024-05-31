e8941b8be8473ef794e3ccf34099e99919b3a68c
Stefan <stefancool00@gmail.com>
2024-05-31 15:49:38 -0700

    Completed Precare page and Booking Page, added instagram label to homepage logo

--- Elevate/views.py
+++ Elevate/views.py
@@ -5,3 +5,9 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def precare(request):
+    return render(request, 'precare.html', {})
+
+def booking(request):
+    return render(request, 'booking.html', {})
